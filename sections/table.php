c5cab7130db452374aeaa2f83f939976c20b5112
eduarchilon <eduarchilon@outlook.es>
2022-09-25 23:42:35 -0300

    style detalle

--- sections/table.php
+++ sections/table.php
@@ -16,7 +16,7 @@ foreach ($pokemones as $pokemon) {
     echo "<tr>
             <td> <img src=" . $imagenPokemon . " alt='' width='50' class='m-auto d-block'></td>
             <td class='pokemon-column-logout'><img src=" . $imagenTIpo . " alt='' width='50' class='m-auto'></td>
-            <td class='pokemon-column-info'><a href='./detallePokemon.php/?param=$identificador'> " . $identificador . "</td>
+            <td class='pokemon-column-info'><a href='./detallePokemon.php/?param=$identificador'> " .($identificador < 10 ? '00'.$identificador  : '0'.$identificador). "</td>
             <td> <a href='./detallePokemon.php/?param=$nombrePokemon'> " . $nombrePokemon . "</td>";
 
     if (isset($_SESSION["pokelog"])) {
